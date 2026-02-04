310fec28d1b6fb6599a64697de867620cfa9b04b
tycon5555 <165697903+tycon5555@users.noreply.github.com>
2026-02-04 08:07:07 -0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,1 @@
-// Supabase Setup
-const SUPABASE_URL = "https://sygyjczuagaifdinjivd.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_Nn-iEz0XfUV-oRwpfkXBaA_eMcdnRSM";
 
-const supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-);
